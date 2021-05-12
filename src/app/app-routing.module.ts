a631e50fb582cecc80f270c6e3335bb3cb4b42fe
DaltonJComer <dalton.comer@outlook.com>
2021-05-12 16:34:07 -0400

    landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { WatchListsComponent } from './watch-lists/watch-lists.component';
 import {SuperheroLandingComponent} from './superhero-landing/superhero-landing.component';
-import { ListComponent } from './list/list.component';
-import {CharacterDetailsComponent} from './character-details/character-details.component'
+import {CharacterDetailsComponent} from './character-details/character-details.component';
+import {ListComponent} from './list/list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BingeCalculatorComponent } from './binge-calculator/binge-calculator.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'landing', pathMatch: "full"},
-  {path:'landing',component: SuperheroLandingComponent},
+  {path: 'landing',component: SuperheroLandingComponent},
   {path: 'list', component: ListComponent},
   {path: 'watchLists', component: WatchListsComponent},
   {path: 'character-details', component: CharacterDetailsComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListComponent } from './list/list.component';
 import {FormsModule} from '@angular/forms';
+import {ListComponent} from './list/list.component';
 import { WatchListsComponent } from './watch-lists/watch-lists.component';
 import { CharacterDetailsComponent } from './character-details/character-details.component';
 import { BingeCalculatorComponent } from './binge-calculator/binge-calculator.component';
